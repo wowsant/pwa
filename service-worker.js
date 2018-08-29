99549d96d02ad401b6ac9540646ce004031e8951
Sant Clair <sant.clair@outlook.com>
2018-08-28 22:53:11 -0300

    Estrutura versão 1.0

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+let cacheName = 'tcc_pwa_v1';
+let filesToCache = [
+	'/',
+	'index.html',
+	'app/css/style.css',
+	'app/js/main.js',
+	'app/css/style.css',
+	'app/lib/bootstrap/css/bootstrap.min.css',
+	'app/lib/jquery/jquery.min.js',
+	'app/css/style.css',
+	'service-worker.js'
+
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[ServiceWorker] Installer');
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', function (e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== cacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
+
+self.addEventListener('fetch', function (e) {
+    console.log('[ServiceWorker] Fetch', e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    );
+});
