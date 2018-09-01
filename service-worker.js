c87ac805db3a50d3c7a45721127215570e1362eb
Sant Clair <sant.clair@outlook.com>
2018-09-01 19:44:43 -0300

    Teste prod

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,15 @@
 let cacheName = 'tcc_pwa_v1';
 let filesToCache = [
 	'/',
-	'index.html',
-	'app/css/style.css',
-	'app/js/main.js',
-	'app/css/style.css',
-	'app/lib/bootstrap/css/bootstrap.min.css',
-	'app/lib/jquery/jquery.min.js',
-	'app/css/style.css',
-	'service-worker.js',
-	'manifest.json'
+	'/index.html',
+	'/app/css/style.css',
+	'/app/js/main.js',
+	'/app/css/style.css',
+	'/app/lib/bootstrap/css/bootstrap.min.css',
+	'/app/lib/jquery/jquery.min.js',
+	'/app/css/style.css',
+	'/service-worker.js',
+	'/manifest.json'
 ];
 
 self.addEventListener('install', function (e) {
