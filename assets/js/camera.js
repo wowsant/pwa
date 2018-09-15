43ec08852c0088a5111b161d42f83a9522e37bec
Sant Clair <sant.clair@outlook.com>
2018-09-15 19:09:31 -0300

    Camere 2.0

---
+++ assets/js/camera.js
@@ -0,0 +1,30 @@
+function getUserMedia(options, successCallback, failureCallback) {
+    var api = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia || navigator.msGetUserMedia;
+    if (api) {
+        return api.bind(navigator)(options, successCallback, failureCallback);
+    }
+}
+
+function getStream (type) {
+    if (!navigator.getUserMedia && !navigator.webkitGetUserMedia &&
+        !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
+        alert('User Media API not supported.');
+        return;
+    }
+
+    var constraints = {};
+    constraints[type] = true;
+    getUserMedia(constraints, function (stream) {
+        var mediaControl = document.querySelector(type);
+
+        if ('srcObject' in mediaControl) {
+            mediaControl.srcObject = stream;
+            mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
+        } else if (navigator.mozGetUserMedia) {
+            mediaControl.mozSrcObject = stream;
+        }
+    }, function (err) {
+        alert('Error: ' + err);
+    });
+}
